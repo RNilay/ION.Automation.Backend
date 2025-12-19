3315aefba5b796d37bd81eb849c257d0e356f8c1
AbhiChavan02 <abhijeetchavan212002@gmail.com>
2025-12-19 18:12:59 +0530

    Added None row in each boughtout item table

--- IonFiltra.BagFilters.Infrastructure/Database/Scripts/1_Main/Inserts/master_data_inserts.sql
+++ IonFiltra.BagFilters.Infrastructure/Database/Scripts/1_Main/Inserts/master_data_inserts.sql
@@ -1,5 +1,6 @@
 INSERT INTO ionfiltrabagfilters.FilterBag 
 (Material, Gsm, Size, Make, Cost, IsDefault) VALUES
+(' ', 0, ' ', 'None', 0, 0),
 ('Polyester oil & water repellent', 550, '150x3660', 'Supreme', 265, 1),
 ('Polyester oil & water repellent', 600, '150x3660', 'Supreme', 285, 0),
 ('100% polyacrylonitrile (PAN) with oil & water repellent finish', 550, '150x3660', 'Supreme', 595, 0),
@@ -8,6 +9,7 @@ INSERT INTO ionfiltrabagfilters.FilterBag
 
 
 INSERT INTO ionfiltrabagfilters.SolenoidValve (Make, Size, Model, Cost, IsDefault) VALUES
+('None', 0, ' ', 0, 0),
 ('Asco', 1.5, 'SCG353G047, 24 VDC', 1550, 0),
 ('Asco', 1.5, 'WPTG353D047, 110VAC / 50', 1825, 1),
 ('Asco', 1.5, 'SCG353G047, 110V AC 50', 1550, 0),
@@ -18,32 +20,39 @@ INSERT INTO ionfiltrabagfilters.SolenoidValve (Make, Size, Model, Cost, IsDefaul
 
 
 INSERT INTO ionfiltrabagfilters.DPTEntity (Make, Model, Cost, IsDefault)
+('None', ' ', 0, 0),
 VALUES ('Siemens', '7MF0340-1DM01-5AM2-Z A00+C11+E01+H01', 36500, 1);
 
 
 INSERT INTO ionfiltrabagfilters.DPGEntity (Make, Model, Cost, IsDefault)
+('None', ' ', 0, 0),
 VALUES ('Dwyer', '2000-250 MM', 4950, 1);
 
 
 INSERT INTO ionfiltrabagfilters.DPSEntity (Make, Model, Cost, IsDefault)
+('None', ' ', 0, 0),
 VALUES ('Wika', 'GM-313-N5-M039 -WW-A-W-C ZZ', 6800, 1);
 
 INSERT INTO ionfiltrabagfilters.PGEntity (Make, Model, Cost, IsDefault)VALUES 
+('None', ' ', 0, 0),
 ('Wika', '213.53.100', 1905, 1),
 ('H Guru', '4 PSSSWS2', 950, 0);
 
 
 INSERT INTO ionfiltrabagfilters.PTEntity (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('Siemens', '7MF0300-1QE01-5AM2-Z A00+C11+E01+H01', 27500, 0),
 ('ABB', '266 HSH-P-S-B-A-7-R1-B6', 30000, 1),
 ('Siemens', '7MF1565-3CA00-1AA1', 11250, 0);
 
 
 INSERT INTO ionfiltrabagfilters.PSEntity (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('Wika', 'PS-903', 2860, 1);
 
 
 INSERT INTO ionfiltrabagfilters.HLDEntity (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('Vega', 'VEGACAP 98', 17000, 0),
 ('Vega', 'VEGACAP 62', 33500, 0),
 ('Sapcon', 'SLC 622', 5000, 1),
@@ -53,32 +62,39 @@ INSERT INTO ionfiltrabagfilters.HLDEntity (Make, Model, Cost, IsDefault) VALUES
 
 
 INSERT INTO ionfiltrabagfilters.RTDEntity (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('Tempsens', 'VEGACAP 98Tempsens Make Rtd With Thermowell (R-504-2-W3-6-316-16-316-½"Npt(M)-½" Npt(M)-50-50-50-150-A-15000)', 5000, 0),
 ('Tempsens', 'Tempsens Make Rtd With Thermowell (Pt-100x2)', 3500, 1);
 
 
 INSERT INTO ionfiltrabagfilters.UTubeManometer (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('IFI', 'IFI', 1100, 1);
 
 
 INSERT INTO ionfiltrabagfilters.ExplosionVent (Make, Size, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('BS&B', '586x920', 14500, 1);
 
 
 INSERT INTO ionfiltrabagfilters.FieldHooter (Make, Size, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('Kheraj', 'SS-100', 10000, 1);
 
 
 INSERT INTO ionfiltrabagfilters.AFREntity (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('Shavo', 'SB60K-4G-M2P-RGLN)', 1950, 1),
 ('Festo', 'LFR-1/2-D-7-MIDI', 4400, 0);
 
 
 INSERT INTO ionfiltrabagfilters.ProxyPulser (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('GOLDEN', 'PP-SB-M30-TF-SS (Standard shaft)', 1570, 1);
 
 
 INSERT INTO ionfiltrabagfilters.ProxymitySwitch (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('TURCK', 'BI10-M30-ADZ3X 7M', 2200, 0),
 ('TURCK', 'NI15-M30-ADZ3X 7M', 2271, 1),
 ('P & F', 'NBB15-30GM50-WS', 4050, 0),
@@ -89,32 +105,39 @@ INSERT INTO ionfiltrabagfilters.ProxymitySwitch (Make, Model, Cost, IsDefault) V
 	
 
 INSERT INTO ionfiltrabagfilters.CableEntity (Make, Size, Type, Cost, IsDefault) VALUES 
+('None', ' ', ' ', 0, 0),
 ('DemoData', '10 Core', 'flexible', 184.48, 1);
 
 
 INSERT INTO ionfiltrabagfilters.ZssController (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('Jayashree', 'RM E211-NITD-CA-22-B30', 3600, 1),
 ('Kana', 'ESS 102-C', 3950, 0);
 
 
 INSERT INTO ionfiltrabagfilters.JunctionBox (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('1model', '20 terminal', 0, 0),
 ('1model', '30 terminal', 0, 1),
 ('1model', '>30', 0, 0);
 
 
 INSERT INTO ionfiltrabagfilters.VibrationTransmitter (Make, Size, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('CTC', 'Vibration Transmitter with L Type Connector, M/LP202-1R2-1E', 18000, 1);
 
 
 INSERT INTO ionfiltrabagfilters.Thermostat (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('', 'Capillary Thermostat (50-300 Deg C)', 3500, 1);
 
 INSERT INTO ionfiltrabagfilters.HopperHeatingPad (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('', '', 0, 1);
 
 
 INSERT INTO ionfiltrabagfilters.CentrifualFan (Make, Volume, Cost, IsDefault) VALUES 
+('None', 0, 0, 0),
 ('Laxmi', 0, 0, 0),
 ('Ritz', 0, 0, 1),
 ('Venti Olde', 0, 0, 0),
@@ -122,34 +145,62 @@ INSERT INTO ionfiltrabagfilters.CentrifualFan (Make, Volume, Cost, IsDefault) VA
 
 
 INSERT INTO ionfiltrabagfilters.ScrewConveyor (Make, Length, Width, Cost, IsDefault) VALUES 
+('None', 0, 0, 0, 0),
 ('Scewtech', 0, 0, 0, 1);
 
 
 INSERT INTO ionfiltrabagfilters.DragChainConveyor (Make, Length, Width, Cost, IsDefault) VALUES 
+('None', 0, 0, 0, 0),
 ('Demech', 0, 0, 0, 1);
 
 
 INSERT INTO ionfiltrabagfilters.RAVGearedMotor (KW, Make, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('0.75 kW, 20 rpm, IE3', 'PBL', 17000, 1);
 
 
 INSERT INTO ionfiltrabagfilters.HardwareEntity (Item, Cost, IsDefault) VALUES 
+('None', 0, 0),
 ('Per bagfilter', 5000, 1);
 
 
 INSERT INTO ionfiltrabagfilters.SStubing (Item, Material, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('Per bagfilter', 'SS304', 5000, 1),
 ('Per bagfilter', 'SS316', 20000, 0);
 
 
 INSERT INTO ionfiltrabagfilters.LTMotor (KW, Efficiency, Framesize, RPM, Make, Cost, IsDefault) VALUES 
+(0, 0, ' ', 0, 'None', 0, 0),
 (0, 0, 'IE2', 0, 'CGL', 0, 1);
 
 
 INSERT INTO ionfiltrabagfilters.TimerEntity (Make, Model, Cost, IsDefault) VALUES 
+('None', ' ', 0, 0),
 ('Kana', 'Solid state', 0, 0),
 ('Kana', 'Microprocessor based', 500, 1),
 ('Kana', 'PLC', 100, 0),
 ('Manik', 'Solid state', 0, 0),
 ('Manik', 'Microprocessor based', 100, 0),
 ('Manik', 'PLC', 500, 0);
+
+
+
+
+
+INSERT INTO ionfiltrabagfilters.MotorisedActuator
+(Make, Model, Cost, IsDefault)
+VALUES
+('None', ' ', 0, 0);
+
+
+INSERT INTO ionfiltrabagfilters.HopperHeatingcontroller
+(Make, Model, Cost, IsDefault)
+VALUES
+('None', ' ', 0, 0);
+
+
+INSERT INTO ionfiltrabagfilters.Thermocouple
+(Make, Model, Cost, IsDefault)
+VALUES
+('None', ' ', 0, 0);
